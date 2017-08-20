65de405388af9c525fa928f81507cfced388fcfd
Etilawin <kimik812@gmail.com>
2017-08-21 01:24:59 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+from nltk.tokenize import sent_tokenize, word_tokenize
+
+# Tokenizing - Word & Sentences tokenizers
+# lecivon and corpras
+# corpra - body of text. ex: medical journals, presidential speeches, English language
+# lexicon - words and their means
+#  Defintion depends on the context
+
+example_text = "Hello there, how are you doing today ? The weather is great and Python is awesome. The sky is blue!"
+
+##print(sent_tokenize(example_text))
+##
+##print(word_tokenize(example_text))
+
+for i in word_tokenize(example_text):
+    print(i)
