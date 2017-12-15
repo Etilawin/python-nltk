f417f8e98c83ea68cbc04acc9938549e5b0eb624
Etilawin <Etilawin@users.noreply.github.com>
2017-12-15 11:43:14 +0100

    Scikit incorporation
    Added many algorithm to prepare a class that classifies much better

--- Scikit-learn_incoorporation.py
+++ Scikit-learn_incoorporation.py
@@ -5,7 +5,13 @@
 import nltk
 import random
 from nltk.corpus import movie_reviews
+
+from nltk.classify.scikitlearn import SklearnClassifier # Wrapper to include the scikit-learn algorithm within the NLTK classifier itself
 import pickle
+
+from sklearn.naive_bayes import MultinomialNB, GaussianNB, BernoulliNB
+from sklearn.linear_model import LogisticRegression, SGDClassifier
+from sklearn.svm import SVC, LinearSVC, NuSVC
 
 documents = [(list(movie_reviews.words(fileid)), category)
             for category in movie_reviews.categories()
@@ -46,9 +52,43 @@ classifier_f = open("naivebays.pickle", 'rb')
 classifier = pickle.load(classifier_f)
 classifier_f.close()
 
-print("Naive Bayes Algo accuracy:", (nltk.classify.accuracy(classifier, testing_set))*100)
+print("Original Naive Bayes Algo accuracy :", (nltk.classify.accuracy(classifier, testing_set))*100)
 classifier.show_most_informative_features(15)
 
-# save_classifier = open("naivebays.pickle", "wb")
-# pickle.dump(classifier, save_classifier)
-# save_classifier.close()
+MNB_classifier = SklearnClassifier(MultinomialNB()) # Creates a classifier (converts it to a nltk classifier)
+MNB_classifier.train(training_set)
+print("MNB_classifier accuracy percent :", (nltk.classify.accuracy(MNB_classifier, testing_set))*100)
+
+# GaussianNB, BernoulliNB
+
+# GaussianNB_classifier = SklearnClassifier(GaussianNB()) # Creates a classifier (converts it to a nltk classifier)
+# GaussianNB_classifier.train(training_set)
+# print("GaussianNB_classifier accuracy percent :", (nltk.classify.accuracy(GaussianNB_classifier, testing_set))*100)
+
+BernoulliNB_classifier = SklearnClassifier(BernoulliNB()) # Creates a classifier (converts it to a nltk classifier)
+BernoulliNB_classifier.train(training_set)
+print("BernoulliNB_classifier accuracy percent :", (nltk.classify.accuracy(BernoulliNB_classifier, testing_set))*100)
+
+
+# LogisticRegression, SGDClassifier
+# SVC, LinearSVC, NuSVC
+
+LogisticRegression_classifier = SklearnClassifier(LogisticRegression()) # Creates a classifier (converts it to a nltk classifier)
+LogisticRegression_classifier.train(training_set)
+print("LogisticRegression_classifier accuracy percent :", (nltk.classify.accuracy(LogisticRegression_classifier, testing_set))*100)
+
+SGDClassifier_classifier = SklearnClassifier(SGDClassifier()) # Creates a classifier (converts it to a nltk classifier)
+SGDClassifier_classifier.train(training_set)
+print("SGDClassifier_classifier accuracy percent :", (nltk.classify.accuracy(SGDClassifier_classifier, testing_set))*100)
+
+SVC_classifier = SklearnClassifier(SVC()) # Creates a classifier (converts it to a nltk classifier)
+SVC_classifier.train(training_set)
+print("SVC_classifier accuracy percent :", (nltk.classify.accuracy(SVC_classifier, testing_set))*100)
+
+LinearSVC_classifier = SklearnClassifier(LinearSVC()) # Creates a classifier (converts it to a nltk classifier)
+LinearSVC_classifier.train(training_set)
+print("LinearSVC_classifier accuracy percent :", (nltk.classify.accuracy(LinearSVC_classifier, testing_set))*100)
+
+NuSVC_classifier = SklearnClassifier(NuSVC()) # Creates a classifier (converts it to a nltk classifier)
+NuSVC_classifier.train(training_set)
+print("NuSVC_classifier accuracy percent :", (nltk.classify.accuracy(NuSVC_classifier, testing_set))*100)
